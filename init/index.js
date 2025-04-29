fc73a2bcf0afc9a9c7c17c628087a8bbcd9ca366
Luvjeet Singh <lt.singh41@gmail.com>
2025-04-29 18:38:54 +0530

    Updated listings init

--- init/index.js
+++ init/index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -11,14 +13,14 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 const initDB = async () => {
   await Listing.deleteMany({});
   initData.data = initData.data.map((obj) => ({
     ...obj,
-    owner: "6808e522e84ff943e34eb4e8",
+    owner: "6810c715355a2b944a844489",
   }));
   await Listing.insertMany(initData.data);
   console.log("Data inserted successfully");
